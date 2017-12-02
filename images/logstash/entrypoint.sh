d6b3e2a1ca1bcbeaa589e5b4de99ca4456f3473a
Ani <sinanajani@outlook.com>
2017-12-02 15:30:08 +0100

    Logging working automatically through logspout and elk stack

---
+++ images/logstash/entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+set -e
+
+curl -XPUT -D- 'http://elasticsearch:9200/.kibana/doc/index-pattern:elk' \
+    -H 'Content-Type: application/json' \
+    -d '{"type": "index-pattern", "index-pattern": {"title": "logstash-*", "timeFieldName": "@timestamp"}}'
+
+
+/usr/local/bin/docker-entrypoint
