1c802e4a4295346c4b00e5805f9040908648f698
Ani Sinanaj <asinanaj@spotify.com>
2022-04-03 00:28:23 +0200

    Removed docker-compose up because handled by makefile

--- bin/dns-mac.sh
+++ bin/dns-mac.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-docker-compose \
-    -f docker-compose-dnsmasq.yml \
-    up -d
-
 if [ ! -f /etc/resolv/test ]; then
     sudo mkdir -pv /etc/resolver
     sudo bash -c 'echo "nameserver 127.0.0.1" > /etc/resolver/test'
